d59cf4d366b9c5b45394074b1fe2f0c267f252ac
Alec Aivazis <alec@aivazis.com>
2019-06-08 03:42:41 -0700

    more learnings on syntax

--- Sumer/src/Geometry.fs
+++ Sumer/src/Geometry.fs
@@ -18,36 +18,40 @@ let ConvexHull2D points =
     // as described here: https://en.wikipedia.org/wiki/Graham_scan#Pseudocode
 
     // let's compute the lowest point, favoring points to the left in case of a tie
-    let p0 = points |> List.reduce (fun (lowest: Vector2) (next: Vector2) ->
-        match (lowest, next) with
-        // if the new item is lower use it
-        | (l, n) when n.y < l.y -> n
-        // if they are at the same height but the new item is to the left use it
-        | (l, n) when n.y = l.y && n.x < l.x -> n
-        // otherwise the lowest still considered lower
-        | _ -> lowest
-    )
+    let p0 = points
+            |> List.reduce (fun (lowest: Vector2) (next: Vector2) ->
+                match (lowest, next) with
+                // if the new item is lower use it
+                | (l, n) when n.y < l.y -> n
+                // if they are at the same height but the new item is to the left use it
+                | (l, n) when n.y = l.y && n.x < l.x -> n
+                // otherwise the lowest still considered lower
+                | _ -> lowest
+            )
 
     // we now have to sort the incoming list of points by polar angle relative to p0
     // if two points have the same polar angle, we have to only include the furthest one
 
     // to do this, we're going to use a map to track if we've seen an angle before
     // and then turn that map into a list which will order the entries by angle in ascending order
-    let mutable polarCoods = ((List.fold (fun (map: Map<float32, Vector2>) (point: Vector2) ->
-        // compute the polar coordinate of this point with p0 and convert into degrees
-        let coords = atan2 (point.y - p0.y ) (point.x - p0.x)
+    let mutable polarCoods =
+        points
+        |>  List.fold (fun (map: Map<float32, Vector2>) (point: Vector2) ->
+                // compute the polar coordinate of this point with p0 and convert into degrees
+                let coords = atan2 (point.y - p0.y ) (point.x - p0.x)
 
-        match map.TryFind(coords) with
-        // we haven't seen the coordinates before so use it
-        | None -> map.Add(coords, point)
-        // we've seen the coordinate and this new point is further away
-        | Some otherPoint when Vector2.Distance(point, p0) > Vector2.Distance(otherPoint, p0) ->
-            // replace the new point over the old point in the map
-            map.Add(coords, point)
-        // anything else we keep life as it is...
-        | _ -> map
-    // turn the map into a list which will sort it in ascending order by key
-    ) Map.empty points) |> Map.toList)
+                match map.TryFind(coords) with
+                // we haven't seen the coordinates before so use it
+                | None -> map.Add(coords, point)
+                // we've seen the coordinate and this new point is further away
+                | Some otherPoint when Vector2.Distance(point, p0) > Vector2.Distance(otherPoint, p0) ->
+                    // replace the new point over the old point in the map
+                    map.Add(coords, point)
+                // anything else we keep life as it is...
+                | _ -> map
+            // turn the map into a list which will sort it in ascending order by key
+            ) Map.empty
+        |> Map.toList
 
     // if the first entry is the origin, remove it
     match polarCoods.Head with
