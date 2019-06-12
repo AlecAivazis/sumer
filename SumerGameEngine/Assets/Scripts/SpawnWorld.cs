e767ca42da41c30bd40854c6a1bca7eca0919aee
Alec Aivazis <alec@aivazis.com>
2019-06-11 21:50:26 -0700

    walls spawn along ground

--- SumerGameEngine/Assets/Scripts/SpawnWorld.cs
+++ SumerGameEngine/Assets/Scripts/SpawnWorld.cs
@@ -53,7 +53,11 @@ public class SpawnWorld : MonoBehaviour
 
         // move the light source to the inside of the building
         Light light = FindObjectsOfType<Light>()[0];
-        light.transform.position = new Vector3(r.Center.x, ground.transform.position.y + 3, r.Center.y);
+        light.transform.position = new Vector3(
+            ground.transform.position.x + r.Center.x,
+            ground.transform.position.y + 3,
+            ground.transform.position.z + r.Center.y
+        );
 
         // the wall from top left to top right
         createWall(corners[0], corners[1], Color.red);
@@ -70,9 +74,21 @@ public class SpawnWorld : MonoBehaviour
         // grab the play boundary defined by the oculus system
         OVRBoundary boundary = OVRManager.boundary;
 
+        // if we are running in a non-VR environment
         if (!boundary.GetConfigured())
         {
-            throw new System.InvalidOperationException("Boundary is not configured");
+            // return the mock bounding box
+            return new Rectangle(
+                System.Tuple.Create(Vector2.right, Vector2.up),
+                new RectangleVertices(
+                    new Vector2(5.0f, -5.0f),
+                    new Vector2(-5.0f, -5.0f),
+                    new Vector2(5.0f, 5.0f),
+                    new Vector2(-5.0f, 5.0f)
+                ),
+                10.0f,
+                10.0f
+            );
         }
 
         // grab the actual points that make up the boundary
@@ -94,6 +110,9 @@ public class SpawnWorld : MonoBehaviour
         // instantiate a copy of the wall prefab
         GameObject newWall = Instantiate(wall, p1, Quaternion.identity) as GameObject;
 
+        // compute the height of the wall object
+        float height = newWall.transform.localScale.y;
+
         // compute the distance between the two points
         float distance = Vector3.Distance(p1, p2);
 
@@ -101,7 +120,7 @@ public class SpawnWorld : MonoBehaviour
         // make the wall face where its going
         newWall.transform.LookAt(p2);
         // put it in the middle
-        newWall.transform.position = p1 + distance / 2 * newWall.transform.forward;
+        newWall.transform.position = p1 + distance / 2 * newWall.transform.forward + (height / 2 * Vector3.up);
 
         Vector3 oldScale = newWall.transform.localScale;
         // stretch the newWall to fill the gap
