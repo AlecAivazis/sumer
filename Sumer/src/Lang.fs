9abd7200a411334775aa735802ed2a4d55178811
Alec Aivazis <alec@aivazis.com>
2019-06-30 01:38:39 -0400

    added arguments to tasks

--- Sumer/src/Lang.fs
+++ Sumer/src/Lang.fs
@@ -7,6 +7,14 @@ type Task(words: string list) =
             // for now the action is just the first word in the command
             words.[0]
 
+    member this.Arguments
+        with get() =
+            match words with
+            // if there is only one element in the list, there are no arguments to the action
+            | [_] -> []
+            // the arguments are everything after the action
+            | _ -> words.[1..words.Length - 1]
+
 // A single command contains many tasks
 type Command(tasks: Task list) =
     // the lists of tasks associated with this command
@@ -14,6 +22,6 @@ type Command(tasks: Task list) =
         with get() : Task list =
             tasks
 
-// takes a string assumed to be spoken text and creates the underlying commands
-// that need to be executed
+// takes a string assumed to be spoken text and creates the underlying command
+// that needs to be executed
 let ParseCommand (sentence: string): Command = Command ([Task (List.ofSeq(sentence.Split ' '))]);
--- SumerTests/tests/Lang.fs
+++ SumerTests/tests/Lang.fs
@@ -9,11 +9,25 @@ type LangTests () =
     [<Test>]
     member this.ParseSimpleCommand() =
         // parse a test command
+        let command = ParseCommand("hello")
+
+        // make sure there is only 1 task
+        Assert.That(command.Tasks.Length, Is.EqualTo(1))
+        let task = command.Tasks.[0]
+
+        // make sure it has the right actions and arguments
+        Assert.That(task.Action, Is.EqualTo("hello"))
+        Assert.That(task.Arguments, Is.EquivalentTo([]))
+
+    [<Test>]
+    member this.ParseCommandWithArgs() =
+        // parse a test command
         let command = ParseCommand("hello world")
 
         // make sure there is only 1 task
         Assert.That(command.Tasks.Length, Is.EqualTo(1))
+        let task = command.Tasks.[0]
 
-        // and that task has the action "hello"
-        Assert.That(command.Tasks.[0].Action, Is.EqualTo("hello"))
-
+        // make sure it has the right actions and arguments
+        Assert.That(task.Action, Is.EqualTo("hello"))
+        Assert.That(task.Arguments, Is.EquivalentTo(["world"]))
