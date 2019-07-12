a06fef670c04d1b6cc2b6e5e90c04d0045931791
Alec Aivazis <alec@aivazis.com>
2019-07-12 14:39:01 -0700

    language server parses strings

--- language/src/command.rs
+++ language/src/command.rs
@@ -2,6 +2,7 @@
 #[derive(Debug, Clone)]
 pub enum Citizen {
     Identifier(String),
+    Str(String),
 }
 
 pub struct Command {
@@ -14,6 +15,8 @@ impl PartialEq for Citizen {
         match (self, other) {
             // if we're comparing two idenfitiers their value has to be the same
             (Citizen::Identifier(s), Citizen::Identifier(o)) => s == o,
+            // if we're comparing two strings their value has to be the same
+            (Citizen::Str(s), Citizen::Str(o)) => s == o,
             // anything else and its not the same
             _ => false,
         }
@@ -21,6 +24,11 @@ impl PartialEq for Citizen {
 }
 
 pub type ParseError = &'static str;
+
+enum Either<A, B> {
+    Left(A),
+    Right(B),
+}
 
 /// parse a string into the corresponding Command
 pub fn parse(cmd: String) -> Result<Command, ParseError> {
@@ -54,7 +62,33 @@ fn parse_arguments(words: &[&str]) -> Result<Vec<Citizen>, ParseError> {
     }
 
     // the words can be a number of different things
-    match words {
+    match words.split_first() {
+        // if the first word is "quote"
+        Some((head, tail)) if *head == "quote" => {
+            // we have to keep consuming until we find another quote
+            // we'll start by building up a left value with everything
+            // and when we find a right value we'll just return that
+            let body =
+                tail.into_iter()
+                    .fold(Either::Left("".to_string()), |prev, next| match prev {
+                        // if we're still looking then look at the next word
+                        Either::Left(previous_value) => match next {
+                            // if the next word is a quote, we're done just return the previous value
+                            &"quote" => Either::Right(previous_value),
+                            // we're still looking
+                            _ => Either::Left(previous_value + " " + next),
+                        },
+                        // we're done looking
+                        v => v,
+                    });
+
+            match body {
+                // if we
+                Either::Right(value) => Result::Ok(vec![Citizen::Str(value[1..].to_string())]),
+                // we didn't find the unquote
+                Either::Left(_) => Result::Err("Did not find matching quote"),
+            }
+        }
         // otherwise treat it as an identifier
         _ => Result::Ok(vec![Citizen::Identifier(words.join(" "))]),
     }
@@ -90,7 +124,16 @@ mod tests {
 
     #[test]
     fn parse_single_argument() {
-        let table = vec![("world", Citizen::Identifier("world".to_string()))];
+        let table = vec![
+            (
+                "hello world",
+                Citizen::Identifier("hello world".to_string()),
+            ),
+            (
+                "quote hello world quote",
+                Citizen::Str("hello world".to_string()),
+            ),
+        ];
 
         for row in table {
             match super::parse("hello ".to_string() + row.0) {
