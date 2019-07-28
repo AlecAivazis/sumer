c4c7bd617433f79fb13a9c790dd53aabc2601de5
Alec Aivazis <alec@aivazis.com>
2019-07-28 16:45:07 -0700

    added failing test for setting state to static value

--- language/SumerLang.Tests/tests/Runtime.fs
+++ language/SumerLang.Tests/tests/Runtime.fs
@@ -2,9 +2,26 @@ namespace SumerLang.Tests
 
 open NUnit.Framework
 open SumerLang.Runtime
+open FsUnit
+
 
 [<TestFixture>]
 type RuntimeTests () =
     [<Test>]
-    member this.ExecuteParser() =
-        ()
+    member this.ExecuteParser_newState() =
+        // we need to test a command that just sets the state
+        let updateParser = fun (input: InputStream) -> Ok(Map.empty)
+        // we do want an operation that sets the new state
+        let updateOperation = NewState "updated!!!"
+
+        // create a runtime to test against
+        let initialRuntime = Runtime(
+                                initialState = "",  
+                                initialCommands = [Command(updateParser, updateOperation)])
+
+        // execute a command
+        match InputStream.FromString "this could be anything" |> initialRuntime.Execute with 
+        | Error(str) -> should equal false true 
+        | Ok(runtime) -> 
+            // this should have updated the internal state of runtime
+            runtime.State |> should equal "updated!!!"
--- language/SumerLang/src/Runtime.fs
+++ language/SumerLang/src/Runtime.fs
@@ -1,27 +1,44 @@
 namespace SumerLang.Runtime
 
-/// Input is the input type for all sumer parsers
-type InputStream = string
+/// InputStream is the input type for all sumer parsers
+type InputStream = 
+    | InputStream of string 
 
+    static member FromString (input: string) =
+        InputStream input
+
+/// Citizens can be passed as an arguments to functions and commands
 type Citizen = 
     | String of string 
-    | Idetifier of string
 
-/// CommandParser is something that might turn a string into a mapping of bindings
-type CommandParser = (InputStream) -> Result<Map<string, Citizen>, string>
+/// OperationBinding is a mapping from binding variable to a citizen to be used in an operation
+type OperationBinding = Map<string, Citizen>
 
-/// Command<ResultT> is hold the necessary information to register a command with a runtime
-type 'ResultT Command = Command of 'ResultT * int
+type ParsingError = string
+/// CommandParser might turn an input stream into a mapping of bindings
+type CommandParser = (InputStream) -> Result<OperationBinding, ParsingError>
+    
+/// Command<'ResultT> associates a parser with an operation to perform if there's a match
+type 'ResultT Command = Command of CommandParser * Operation<'ResultT>
 
-/// Runtime<StateT> is responsible for handing incoming strings, checking them against
-/// the list of known commands and executing the commands against its internal state.
-type Runtime<'StateT>(initialState: 'StateT, initialCmds: List<Command<'StateT>>) = 
+/// Operation<'T> is the description of a mutation on an object of type 'T
+and 'T Operation = 
+    /// An operation that instructs the runtime to change its state to a specific value
+    | NewState of 'T
+
+
+/// Runtime<StateT> handles incoming strings, checking them against the list of known commands 
+/// and executing the commands against its internal state.
+type Runtime<'StateT>(initialState: 'StateT, initialCommands: List<Command<'StateT>>) = 
     // make the state available
     member this.State = initialState
     // as well as the list of commands
-    member this.Commands = initialCmds
+    member this.Commands = initialCommands
 
     /// Execute takes an InputStream and might update its internal state if there is 
     /// a matching command
-    member this.Execute (command: InputStream): Result<unit, string> =
-        Ok(())
+    member this.Execute (command: InputStream): Result<Runtime<'StateT>, string> =
+
+
+        // nothing went wrong, nothing was updated
+        Ok(this)
